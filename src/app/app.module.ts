71093f2cdfffb113a1c06e33bf8bc3c8a4b8423e
Catriel Pérez <perezcatriell@gmail.com>
2022-03-08 15:36:06 -0300

    Agregando estilos y componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ProfilePictureAndBackgroundComponent } from './components/profile-picture-and-background/profile-picture-and-background.component';
+import { TitleComponent } from './components/title/title.component';
+import { PresentationComponent } from './components/presentation/presentation.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, ProfilePictureAndBackgroundComponent],
+  declarations: [AppComponent, HeaderComponent, ProfilePictureAndBackgroundComponent, TitleComponent, PresentationComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
