142fa46de30fa26200ca2650dd076b9f52bdba95
perezcatriel <perezcatriel@gmail.com>
2022-03-17 14:01:44 -0300

    Agregando String interpolation {{}}, event binding [] y data binding [()]

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -3,20 +3,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  password = 'catriel1';
+  email = 'perezcatriel@gmail.com';
+  btnDisabled = 'true';
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  saludo(password: any, email: any): void {
+    alert(`password: ${password} \n email: ${email}`);
   }
-
-  saludo(): void {
-    console.log('Hi');
-  }
-
-
 }
-
-
